8a794141db348ceaf98a69b7f97c5abe31ef6c6f
BeshTeR <avesh.net@bk.ru>
2022-07-28 19:10:33 +0300

    add chain:is_chain/1

--- libs/chain.erl
+++ libs/chain.erl
@@ -7,7 +7,7 @@
 -module(chain).
 
 %% API
--export([make/1, make/2, make/3, to_string/1]).
+-export([make/1, make/2, make/3, is_chain/1, to_string/1]).
 
 %% Tests
 -include("tests/chain_tests.erl").
@@ -40,12 +40,29 @@ make(N, L1) -> make(N, L1, []).
     L2     :: [pos_integer()],
     Return :: chain() | {error, bad_format}.
 
-make(N, L1, L2) when is_integer(N), N > 0 ->
-    case is_list_integer(L1) andalso is_list_integer(L2) of
-        true -> {N, L1, L2};
+make(N, L1, L2) ->
+    C = {N, L1, L2},
+    case is_chain(C) of
+        true -> C;
         false -> {error, bad_format}
-    end;
-make(_, _, _) -> {error, bad_format}.
+    end.
+
+%% -----------------------------------------------------------------------------
+%% @doc Это цепная дробь ?
+%% @end
+%% -----------------------------------------------------------------------------
+-spec is_chain(C) -> Return when
+    C      :: any(),
+    Return :: boolean().
+
+is_chain({N, L1, L2}) when is_integer(N), N > 0 ->
+    is_list_integer(L1) andalso is_list_integer(L2);
+is_chain(_) -> false.
+
+%% это список целых положительных чисел ?
+is_list_integer([]) -> true;
+is_list_integer([H|T]) when is_integer(H), H > 0 -> is_list_integer(T);
+is_list_integer(_) -> false.
 
 %% -----------------------------------------------------------------------------
 %% @doc Преобразовать цепную дробь в строку
@@ -72,10 +89,3 @@ to_string([H], Str) ->
     Str ++ "1/" ++ integer_to_list(H);
 to_string([H|L], Str) ->
     to_string(L, Str ++ "1/" ++ integer_to_list(H) ++ "+").
-
-%% Private ---------------------------------------------------------------------
-
-%% это список целых положительных чисел ?
-is_list_integer([]) -> true;
-is_list_integer([H|T]) when is_integer(H), H > 0 -> is_list_integer(T);
-is_list_integer(_) -> false.
