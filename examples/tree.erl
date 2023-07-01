b4d2dbd931d6654897808a283d69f4a190187001
Олег Муравьев <106950511+BeshTeR@users.noreply.github.com>
2023-07-01 16:59:03 +0300

    update

---
+++ examples/tree.erl
@@ -0,0 +1,37 @@
+-module(tree).
+-export([empty/0, insert/3, lookup/2, has_value/2]).
+
+empty() -> 
+    {node, nil}.
+
+insert(Key, Val, {node, nil}) -> 
+    {node, Key, Val, empty(), empty()};
+insert(Key, Val, {node, Key, _, L, R}) ->
+    {node, Key, Val, L, R};
+insert(Key, Val, {node, K, V, L, R}) when Key < K ->
+    {node, K, V, insert(Key, Val, L), R};
+insert(Key, Val, {node, K, V, L, R}) when Key > K ->
+    {node, K, V, L, insert(Key, Val, R)}.
+
+lookup(_, {node, nil}) ->
+    undefined;
+lookup(Key, {node, Key, Val, _, _}) ->
+    {ok, Val};
+lookup(Key, {node, K, _, L, _}) when Key < K ->
+    lookup(Key, L);
+lookup(Key, {node, _, _, _, R}) ->
+    lookup(Key, R).
+
+has_value(Val, Tree) ->
+    try has_value({Val, Tree}) of
+        false -> false
+    catch
+        true -> true
+    end.
+has_value({_, {node, nil}}) ->
+    false;
+has_value({Val, {node, _, Val, _, _}}) ->
+    throw(true);
+has_value({Val, {node, _, _, L, R}}) ->
+    has_value({Val, L}),
+    has_value({Val, R}).
